f7d5b40315250c5af458df377323c86819df3c90
davidtao212 <chengtao06@gmail.com>
2026-07-31 13:13:29 +0800

    Add yuxin2 train config.

--- src/openpi/training/misc/deerbaby_config.py
+++ src/openpi/training/misc/deerbaby_config.py
@@ -171,6 +171,40 @@ def get_deerbaby_configs():
             keep_period=30_000,
         ),
 
+        TrainConfig(
+            name="pi0_open_close_door_yuxin2",
+            model=pi0_config.Pi0Config(),
+            data=LeRobotDeerbabyDataConfig(
+                repo_id="silverlife/open_close_door_yuxin2",
+                base_config=DataConfig(prompt_from_task=True),
+                is_mobile=True,
+                base_view='camera_high',
+                third_view='camera_front',
+                assets=AssetsConfig(asset_id="deerbaby"),
+            ),
+            weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi0_base/params"),
+            num_train_steps=30_000 + 100,
+            save_interval=1000,
+            keep_period=30_000,
+        ),
+
+        TrainConfig(
+            name="pi0_open_close_door_yuxin_mix",
+            model=pi0_config.Pi0Config(),
+            data=LeRobotDeerbabyDataConfig(
+                repo_id="silverlife/open_close_door_yuxin_mix",
+                base_config=DataConfig(prompt_from_task=True),
+                is_mobile=True,
+                base_view='camera_high',
+                third_view='camera_front',
+                assets=AssetsConfig(asset_id="deerbaby"),
+            ),
+            weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi0_base/params"),
+            num_train_steps=30_000 + 100,
+            save_interval=1000,
+            keep_period=30_000,
+        ),
+
         # Inference Caihong configs
         TrainConfig(
             name="caihong_v2",
